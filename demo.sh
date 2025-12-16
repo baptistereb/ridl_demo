47fd1974cfd7d3e6dd9d8e28abf7db65530106da
Baptiste <baptiste.rebillard@outlook.fr>
2025-12-16 11:51:06 +0100

    cleaning a bit

--- demo.sh
+++ demo.sh
@@ -14,12 +14,10 @@ info() {
 
 log "Opening terminals..."
 
-SSH_OPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-
 xterm -T "VICTIM" -bg black -fg white -fa 'Monospace' -fs 15 -geometry 67x50-0+0 -e \
-    "echo 'Connecting to VICTIM...'; ssh $SSH_OPTS victim@server; bash" &
+    "cd scripts && make; bash" &
 
 xterm -T "ATTACKER" -bg black -fg white -fa 'Monospace' -fs 15 -geometry 67x50+0+0 -e \
-    "echo 'Connecting to EVE...'; ssh $SSH_OPTS attack@server; bash" &
+    "cd scripts; bash" &
 
 log "Demo ready!"
